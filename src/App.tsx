bbd7b0f556d54850b70aace4960a2c667ee47cd4
Mahati Tadepalli <mahati0215@gmail.com>
2026-05-04 14:40:54 -0400

    Camera works now

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import HandTracking from './components/HandTracking'
+import React, { useRef, useEffect} from 'react';
 import './App.css'
 
 function App() {
 
-  return (
-    <h1>CAMERA</h1>
+   //CAMERA
 
-    //make the camera work
-    //implement hand tracking
-    
-  )
+	const videoRef = useRef<HTMLVideoElement>(null);
+
+   const getVideo = () =>{
+      navigator.mediaDevices.getUserMedia({
+         video:{width: 500, height: 500}
+      })
+      .then(stream => {
+         let video = videoRef.current;
+            video.srcObject = stream;
+            video.play();
+      })
+      .catch(err =>{
+         console.error(err);
+      })
+   }
+
+   useEffect(() => {
+      getVideo;
+   }, [videoRef])
+
+
+   //
+
+
+	return (
+		<div className='App'>
+			<h1>CAMERA</h1>
+
+			{/* make the camera work (useRef REACT HOOK) */}
+			<div className="camera">
+				<video ref={videoRef}></video>
+			</div>
+		</div>
+	)
 }
 
 export default App
